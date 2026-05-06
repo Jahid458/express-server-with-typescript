b37fc34a9c93474e53206b1c7e53de4e87d2c062
Jahid458 <Jahidul898@outlook.com>
2026-05-06 11:18:31 +0600

    Added single user get id

--- src/server.ts
+++ src/server.ts
@@ -97,6 +97,34 @@ app.get("/users", async (req: Request, res: Response) => {
   } 
 });
 
+
+// dynamic user id
+app.get("/users/:id", async (req:Request, res:Response)=> {
+  // console.log(req.params.id);
+  try {
+      const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+      if(result.rows.length === 0){
+          res.status(404).json({
+            success: false,
+            message: 'User not found'
+          })
+      }else{
+          res.status(200).json({
+            success: true,
+            message: 'User fetched successfully',
+            data: result.rows[0]
+          })
+      }
+  }
+  catch (err: any) {    
+      res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err
+    });
+  }   
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
